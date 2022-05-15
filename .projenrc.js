e7900afe21761f85f763d766099417ca0f0c5589
Neil Kuan <guan840912@gmail.com>
2022-05-15 08:46:08 +0800

    chore: add ignore file

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.22.0',
   defaultReleaseBranch: 'main',
   name: 'telegram-bot-sample',
-  gitignore: ['venv', '.env', 'cdk.context.json', 'a.py'],
+  gitignore: ['venv', '.env', 'cdk.context.json', 'a.py', 'sample.json'],
   depsUpgradeOptions: {
     ignoreProjen: false,
     workflowOptions: {
